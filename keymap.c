40e04528e894f2624f421ae2377799de554ba2bd
Vihaan Vinoth <vertialX1000@outlook.com.au>
2026-09-08 22:39:22 +1000

    add keymap.c

---
+++ keymap.c
@@ -0,0 +1,10 @@
+#include QMK_KEYBOARD_H
+#include "process_steno.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        STN_N1, STN_N2, STN_N3, STN_N4, STN_N5, STN_N6, STN_N7, STN_N8, STN_N9, STN_NA, STN_NB, STN_NC,
+        STN, S1, STN_TL, STN_PL, STN_HL, STN_ST1, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR, STN_ZR, STN_FN,
+        STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2, STN_A, STN_O, STN_E, STN_U, STN_RR, STN_BR, STN_GR
+    )
+};
